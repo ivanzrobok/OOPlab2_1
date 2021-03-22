4b8f11bf653bd880efe03b0d3d6d968f0320f77c
Zobok Ivan <ivan.zrobok@hotmail.com>
2021-03-22 14:36:26 +0200

    first

---
+++ OOP.Lab2.1/Program.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP.Lab2._1
+{
+    public class Time
+    {
+        public Time() { }
+        public Time(int first, int second)
+        {
+            this.first = first;
+            this.second = second;   
+        }
+        
+        private int first;
+        private int second;
+        public float Minutes()
+        {
+            return first * 60 + second;
+        }
+        public void Input()
+        {
+            string first, second;
+            Console.WriteLine("Введіть перше число: ");
+            first = Console.ReadLine();
+            Console.WriteLine("Введіть друге число: ");
+            second = Console.ReadLine();
+
+            bool Isdigit1 = first.Length == first.Where(c => char.IsDigit(c)).Count();
+            bool Isdigit2 = second.Length == second.Where(c => char.IsDigit(c)).Count();
+
+            if (Isdigit1 && Isdigit2)
+            {
+                this.first = Convert.ToInt32(first);
+                this.second = Convert.ToInt32(second);
+                Console.WriteLine("Ви ввели правильні числа");
+            }
+            else Console.WriteLine("Числа неправильні");
+
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Кількість хвилин в {this.first} год. i {this.second} хв. = {Minutes()}");
+        }
+    }
+
+   
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Time time = new Time();
+            time.Input();
+            time.Minutes();
+            time.Display();
+        }
+    }
+}
---
+++ lab2.1Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP.Lab2._1;
+using System;
+
+namespace lab2._1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Time time = new Time(2, 30);
+            Assert.AreEqual(time.Minutes(), 150);
+        }
+    }
+}
